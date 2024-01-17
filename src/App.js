97789d1109b98dcb72099d3a19f8207dca8b329b
AviPeltz <aj.peltz@gmail.com>
2024-01-17 14:06:41 -0800

    more react learning

--- src/App.js
+++ src/App.js
@@ -10,8 +10,25 @@ function Recipe({ drinkers }){
     </ol>
   );
 }
+function Cup({ guest }){
+  return(
+    <h1 className='text-md font-normal text-blue-500'>Tea cup for guest #{guest}</h1>
+  );
+}
+
+// Why doesnt this work?
+function TeaSet(guests){
+  const guestItems = guests.map(guest => <Cup guest={guest} />);
+  return(
+    <div className='container'>
+      {guestItems}
+    </div>
+  );
+}
 
 export default function App() {
+  var guests = ['1','2','3'];
+  const guestItems = guests.map(guest => <Cup guest={guest} />);
   return (
     <section className='mx-auto grid justify-center'>
       <div className='container p-4'>
@@ -24,6 +41,12 @@ export default function App() {
       <div className='container p-3'>
         <h2 className='text-xl font-semibold'>For a gathering</h2>
         <Recipe drinkers={4} />
+      </div>
+      <div className='container p-3'>
+        <h2 className='text-xl font-semibold'>Product List for Tea Set:</h2>
+        <div className='container'>
+          {guestItems}
+        </div>
       </div>
     </section>
   );
