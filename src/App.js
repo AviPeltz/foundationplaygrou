f5a80dfebe9329182738b85abb8140a827e37c85
AviPeltz <aj.peltz@gmail.com>
2024-01-17 13:46:38 -0800

    recipe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import GPTPlayground from './GPTPlayground';
 import './App.css';
+
+function Recipe({ drinkers }){
+  return(
+    <ol className='p-4 list-decimal'>
+      <li>Boil {drinkers} cups of water.</li>
+      <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.</li>
+      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+    </ol>
+  );
+}
 
 export default function App() {
   return (
-    <div className="App">
-      <GPTPlayground />
-    </div>
+    <section className='mx-auto grid justify-center'>
+      <div className='container p-4'>
+        <h1 className='text-3xl font-bold'>Spiced Chai Recipe</h1>
+      </div>
+      <div className='container p-3'>
+        <h2 className='text-xl font-semibold'>For two</h2>
+        <Recipe drinkers={2} />
+      </div>
+      <div className='container p-3'>
+        <h2 className='text-xl font-semibold'>For a gathering</h2>
+        <Recipe drinkers={4} />
+      </div>
+    </section>
   );
 }
