e128f2f0cd47cebe998f54313c9e436133ca319c
AviPeltz <aj.peltz@gmail.com>
2024-02-28 07:24:27 -0800

    more Project page updates

---
+++ src/assets/PixelWiseIcon.js
@@ -0,0 +1,17 @@
+
+export default function PixelWiseIcon(){
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <g clip-path="url(#clip0_608_8639)">
+            <path d="M10.5918 11.6292L15.2977 8.84344L20.0036 11.636V17.2142L15.2977 20L10.5918 17.2075" fill="#AEFFAA"/>
+            <path d="M5.28543 8.37176L5.21484 2.83215L9.85014 0L14.556 2.70649L14.6266 8.2461L9.99131 11.0782" fill="#AAE5FF"/>
+            <path d="M9.41176 17.2075L4.70588 20L0 17.2142V11.636L4.70588 8.84344L9.41176 11.6292V17.2075Z" fill="#FFD4F0"/>
+        </g>
+        <defs>
+            <clipPath id="clip0_608_8639">
+            <rect width="20" height="20" fill="white"/>
+            </clipPath>
+        </defs>
+        </svg>
+    );
+}
--- src/components/Pages/ProjectsPage.js
+++ src/components/Pages/ProjectsPage.js
@@ -1,4 +1,6 @@
 import TensorIcon from "../../assets/TensorIcon";
+import PixelWiseIcon from "../../assets/PixelWiseIcon";
+import EllipseIcon from "../../assets/EllipseIcon";
 import "../../App.css";
 import { Button } from "../Buttons";
  /**  -ProjectNav
@@ -45,34 +47,33 @@ const navItems =[
 const projects = [
     {
         id: 0,
-        name: "myFirstProject",
-        type: "Instance Segmentation",
+        name: "My FirstProject",
+        type: "PIXELWISE LABELS",
         lastEdited: "January 27, 2024",
         projectImage: "https://s3-alpha-sig.figma.com/img/3d21/1975/1baf0e0aa42a2ccacdd502ce48e9ecde?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RTsHe5LNaG6HIFDvJHW79xQFbNxM2tR3Ft8vXfIXqG5cA5Pg5XwbHa0MiK-yWG31VBeQRuAmnBltwdyrG5mjxXoRiSv1r5zJx1eCOi~XQCObw1FuFmYQhSjHQA87tGjUMb99sT4XQy0GV353ed9MWMhk92fTCfzOby1AsCtLwyED4AacRTtnjKolWq~wwMnR8bq9nAwZ5AGmHoiMStZKHWfOZaX40yowZ3Wj5L9rtx85h~Y9Vqwt-d5Se4AF-I2dgJrI2ay3LDOYDaF-nTYwzyWwuwYisVQzYWaY5YcsnZL3VrH52adbiAtydpZgoGH5qOuy5Jk4Iz2~6G154MfJcA__",
 
     }
-]
+];
 export default function ProjectsPage(){
     return(
         <>
-            <ProjectNav />
+            <ProjectNav navItems={navItems}/>
             <ProjectsContent>
                 <ProjectsHeading />
                 <ProjectsGallery>
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
+                    <ProjectCard project={projects[0]} />
                 </ProjectsGallery>
             </ProjectsContent>
 
         </>
     );
 }
-export function ProjectNav(){
+export function ProjectNav({ navItems }){
     return(
         <nav className='projectNav'>
             <NavBrand />
+            <NavLinks navItems={navItems} />
+            <AvatarDropdown />
         </nav>
     );
 }
@@ -86,14 +87,16 @@ export function NavBrand(){
 export function NavLinks({ navItems }){
     return(
         <div className="navLinks">
-            {}
+            {navItems.map((item) => (
+                item.isSelected ? <a key={item.id} href="#" className="navLink_selected">{item.name}</a> : <a key={item.id} href="#" className="navLink">{item.name}</a>
+            ))}
         </div>
     );
 }
 export function AvatarDropdown({ Name, AvatarURL, navItems }){
     return(
-    <div>
-        <p>{Name}</p>
+    <div className="avatarDropdown">
+        <p>Avi Peltz</p>
     </div>
     );
 }
@@ -127,9 +130,14 @@ export function ProjectsGallery({ children, projects }){
 export function ProjectCard({ project }){
     return(
     <div className="projectCard">
+        <img className="projectCard" src={project.projectImage} alt="projectImage" />
         <div className="projectCardContent">
-            <h2>My First Project</h2>
-            <p>last edited: '1 month ago'</p>
+            <h3 className="projectCardContent">{project.type}</h3>
+            <PixelWiseIcon />
+            <h2 className="projectCardContent">{project.name}</h2>
+            <EllipseIcon />
+            <p className="projectCardContent">last edited: '1 month ago'</p>
+
         </div>
     </div>
     );
